3c367694f2b937e6bd2a1d0861b1372a0bdd3ec4
Gabriel <l77gabrielgh77@hotmail.com>
2025-05-13 23:39:24 -0300

    aula04

---
+++ desafio03.py
@@ -0,0 +1,6 @@
+print('=========== DESAFIO 03 ===========')
+
+n1 = int(input('Informe o primeiro número: '))
+n2 = int(input('Informe o segundo número: '))
+
+print('A soma entre os números informado é ', n1 + n2)
