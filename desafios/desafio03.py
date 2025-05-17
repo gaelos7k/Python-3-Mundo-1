b65e392707ffe25661555c840e7e5da457cc5fac
Gabriel <l77gabrielgh77@hotmail.com>
2025-05-16 23:03:56 -0300

    salvando

--- desafios/desafio03.py
+++ desafios/desafio03.py
@@ -3,4 +3,4 @@ print('=========== DESAFIO 03 ===========')
 n1 = int(input('Informe o primeiro número: '))
 n2 = int(input('Informe o segundo número: '))
 
-print('A soma entre os números informado é ', n1 + n2)
+print('A soma entre {} e {} é {} '.format(n1, n2, n1+n2))
---
+++ desafios/desafio4.py
@@ -0,0 +1,9 @@
+entrada = input('Informe algo: ')
+
+print(type(entrada))
+print(entrada.isalnum())
+print(entrada.isalpha())
+print(entrada.isnumeric())
+print(entrada.isdecimal())
+print(entrada.isascii())
+print(entrada.isdigit())
