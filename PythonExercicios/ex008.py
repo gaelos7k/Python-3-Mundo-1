75ff2f34993e8b94f049201dcad5464a76801c44
Gabriel <l77gabrielgh77@hotmail.com>
2025-05-20 21:11:17 -0300

    ex008 att

--- PythonExercicios/ex008.py
+++ PythonExercicios/ex008.py
@@ -1,7 +1,15 @@
 valor = float(input('Informe um valor em metros: '))
 
+decimetro = valor * 10
 centimetros = valor * 100
 milimetros = valor * 1000
+quilometros = valor / 1000
+hectometro = valor / 100
+decametro = valor / 10
 
+print('O valor informado em decímetro é {}'.format(decimetro))
 print('O valor informado em centimetros é {}'.format(centimetros))
 print('O valor informado em milimetros é {}'.format(milimetros))
+print('O valor informado em quilômetro é {}'.format(quilometros))
+print('O valor informado em hectomêtro é {}'.format(hectometro))
+print('O valor informado em decametro é {}'.format(decametro))
